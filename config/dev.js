ceb9609842afc2c36a8397217aa7cd274706f7f9
yifatre <yifat.reg@gmail.com>
2024-04-01 18:11:00 +0300

    Connect to atlas

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 export default {
-    dbURL: 'mongodb://127.0.0.1:27017',
+    // dbURL: 'mongodb://127.0.0.1:27017',
+    dbURL: 'mongodb+srv://yifatreg:7V2OSasRi4mZojn7@toys.syo3ooy.mongodb.net/?retryWrites=true&w=majority&appName=toys',
     dbName: 'toy_db',
 }
