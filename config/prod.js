3ba13a55aad98b33f0ffbd558eeed28717a70b03
yifatre <yifat.reg@gmail.com>
2024-04-01 20:21:54 +0300

    Fix db link in production

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-    dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-    dbName: 'tester_db',
+    dbURL: 'mongodb+srv://yifatreg:7V2OSasRi4mZojn7@toys.syo3ooy.mongodb.net/?retryWrites=true&w=majority&appName=toys',
+    dbName: 'toy_db',
 }
